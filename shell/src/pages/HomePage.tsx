55dfe922014bbc16291a25d04fdd4a50d91aa796
jardenna <hbjornum@gmail.com>
2024-06-03 12:12:23 +0200

    load remote

--- shell/src/pages/HomePage.tsx
+++ shell/src/pages/HomePage.tsx
@@ -1,8 +1,22 @@
-import { FC } from 'react';
+import { FC, Suspense, lazy } from 'react';
+
+import { loadRemote } from '@module-federation/enhanced/runtime';
+import App1Btn from 'app1/App1Btn';
+
+const NavigationBtn = lazy(
+  () =>
+    loadRemote<{ default: typeof App1Btn }>('app1/App1Btn', {
+      from: 'runtime',
+    }) as Promise<{ default: typeof App1Btn }>
+);
 
 const HomePage: FC = () => (
   <div>
     <h1>Home</h1>
+
+    <Suspense fallback="Loading App btn...">
+      <NavigationBtn />
+    </Suspense>
   </div>
 );
 export default HomePage;
