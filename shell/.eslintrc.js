5ebda310f51a9188180780b4560129812369057f
jardenna <hbjornum@gmail.com>
2024-06-04 13:31:16 +0200

    lint

--- shell/.eslintrc.js
+++ shell/.eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
     '@typescript-eslint/no-duplicate-enum-values': 'warn',
     '@typescript-eslint/no-empty-interface': 'warn',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
 
     'import/no-extraneous-dependencies': [
       'error',
